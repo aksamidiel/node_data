30220a59c93b52012a302090dd18850ce96be571
ridus norman <C:\Users\User\AppData\Roaming\The Bat!>
2021-07-29 15:37:49 +0300

    create connection to mongoBD

--- app.js
+++ app.js
@@ -7,9 +7,44 @@ const categoryRoutes = require('./routes/category') //импорт роута
 const orderRoutes = require('./routes/order') //импорт роута
 const positionRoutes = require('./routes/position') //импорт роута
 //const bodyParser = require("body-parser") //парсер устарел(по умолчанию уже есть в express)
+//const mongoose = require('mongoose') //подключение базы данных
+const keys = require('./config/keys')
+
+
 
 const cors = require('cors') // для ответа клиенту с разных доменов
 const morgan = require('morgan') // логирование запросов
+
+
+const { MongoClient } = require("mongodb");
+
+
+// Connection URI
+// Create a new MongoClient
+const client = new MongoClient(keys.mongoURI);
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+//mongoose.connect(keys.mongoURI)
+//.then(() => {
+//    console.log('MongoDB connected. ')
+//})
+//.catch(err =>{
+//    console.log('error ')
+//})
+
 
 const app = express()
 
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// файл для хранения конфигураций
+
+module.exports = {
+    mongoURI: 'mongodb+srv://aksamidiel:RdbrUR38@cluster0.4k66j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
